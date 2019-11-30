7b6424e97dfba8c92b084f2c3f7beeb03e037965
Gerd Breidenstein <Gerd.Breidenstein@gmail.com>
2019-11-30 19:04:06 +0100

    Test

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+
+namespace HelloWorld_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
+
